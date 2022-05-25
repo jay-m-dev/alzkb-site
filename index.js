95ec88798ec9b055e1aca608ccefe8db141f400c
Jay M <jmoran2@wgu.edu>
2022-05-25 11:39:51 -0700

    CORS config options

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ app.set('view engine', 'ejs')
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "http://3.93.173.231:7474"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Methods","GET, POST, OPTIONS"); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Credentials", true);
     next();
   });
 
