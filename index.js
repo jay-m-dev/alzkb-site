772256a414ea9cab9373bd6c0b2dec9070da5e6c
Jay M <jmoran2@wgu.edu>
2022-05-25 11:32:02 -0700

    updated cors origin

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(express.static('public'))
 app.set('view engine', 'ejs')
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://3.93.173.231"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Origin", "http://3.93.173.231:7474"); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
