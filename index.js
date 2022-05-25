8399e90330854e338d54603ec2e601bc56ec9ebe
Jay M <jmoran2@wgu.edu>
2022-05-25 12:11:02 -0700

    put back headers and boolean string

--- index.js
+++ index.js
@@ -6,11 +6,12 @@ const ejs = require('ejs')
 app.use(express.static('public'))
 app.set('view engine', 'ejs')
 
+
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header("Access-Control-Allow-Methods","GET, HEAD, POST, OPTIONS, PUT"); // update to match the domain you will make the request from
-    // res.header("Access-Control-Allow-Headers", "cache-control, pragma, Access-Control-Allow-Headers, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method, Access-Control-Request-Headers");
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Methods","GET,HEAD,POST,OPTIONS,PUT"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method, Access-Control-Request-Headers");
     next();
   });
 
