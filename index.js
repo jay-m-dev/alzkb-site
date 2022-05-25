8d2645e37b92ed3c068edc0925af41e724403608
Jay M <jmoran2@wgu.edu>
2022-05-25 11:49:23 -0700

    CORS cache-control, pragma

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Credentials", "true");
     res.header("Access-Control-Allow-Methods","GET, HEAD, POST, OPTIONS, PUT"); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method, Access-Control-Request-Headers");
+    res.header("Access-Control-Allow-Headers", "cache-control, pragma, Access-Control-Allow-Headers, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method, Access-Control-Request-Headers");
     next();
   });
 
