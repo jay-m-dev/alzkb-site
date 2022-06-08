668120af68dec7f6b697bbac97020aa0c57d163d
Jay M <jmoran2@wgu.edu>
2022-06-08 16:03:05 -0700

    docker compose build dev and prod

--- build.sh
+++ build.sh
@@ -1,15 +1,32 @@
 #!/bin/bash
 
-docker image prune
+if [[ $1 == "prod" ]] || [[ $1 == "dev" ]]; then
+  echo "Building $1..."
+else
+  echo "USAGE: ./build.sh [OPTION] [ARG]"
+  echo "OPTIONS:"
+  echo " prod    - production environment"
+  echo " dev     - development environment"
+  echo "ARGS:"
+  echo " up      - Start containers "
+  exit
+fi
+
 
 if [[ $1 == "prod" ]]; then
   echo "copying prod config files"
-  cp app/config/prod.env app/config/alzkb.env
+  cp ./app/config/.env.prod ./.env # .env file is used by docker compose
   cp nginx/alzkb.prod.conf nginx/alzkb.conf
 else
   echo "copying dev config files"
-  cp app/config/dev.env app/config/alzkb.env
+  cp ./app/config/.env.dev ./.env # .env file is used by docker compose
   cp nginx/alzkb.dev.conf nginx/alzkb.conf
 fi
 
-docker compose -f ./docker-compose.yml up -d --build
+echo "Building images..."
+docker compose build
+
+if [[ $2 == "up" ]]; then
+  echo "Starting containers..."
+  docker compose up -d
+fi
