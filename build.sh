00482b9b7f480e2ba1aa83890d65bd8f277fd925
Jay M <jmoran2@wgu.edu>
2022-06-07 17:19:16 -0700

    added nginx prod and dev conf files

--- build.sh
+++ build.sh
@@ -3,11 +3,13 @@
 docker image prune
 
 if [[ $1 == "prod" ]]; then
-  echo "copying prod.env file"
+  echo "copying prod.env files"
   cp app/config/prod.env app/config/alzkb.env
+  cp nginx/alzkb.prod.conf nginx/alzkb.conf
 else
   echo "copying dev.env file"
   cp app/config/dev.env app/config/alzkb.env
+  cp nginx/alzkb.dev.conf nginx/alzkb.conf
 fi
 
 docker compose -f ./docker-compose.yml up -d --build
