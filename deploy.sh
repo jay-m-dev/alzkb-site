8c5cdee2100542a90e16b017c4cd64be65973ab7
Jay M <jmoran2@wgu.edu>
2022-06-09 15:33:07 -0700

    dev and prod nginx conf copy

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,10 @@
 
 if [[ $1 == "prod" ]]; then
   cp ./app/config/.env.prod ./.env
+  cp nginx/alzkb.prod.conf nginx/alzkb.conf
 else
   cp ./app/config/.env.dev ./.env
+  cp nginx/alzkb.dev.conf nginx/alzkb.conf
 fi
 
 docker compose up -d
