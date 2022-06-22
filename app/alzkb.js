dd8c5b99b9138117a6f174849ffdf2663cf53309
Jay Moran <jmoran2@wgu.edu>
2022-06-22 14:11:52 -0700

    added compression to app

--- app/alzkb.js
+++ app/alzkb.js
@@ -4,11 +4,13 @@ const config = require('./config/config')
 
 const app = express()
 const cors = require('cors')
+const compression = require('compression')
 // const helmet = require('helmet')
 // const ejs = require('ejs')
 require('ejs')
 
 app.use(cors())
+app.use(compression())
 app.use(express.static('public'))
 app.set('view engine', 'ejs')
 // app.use(helmet())
